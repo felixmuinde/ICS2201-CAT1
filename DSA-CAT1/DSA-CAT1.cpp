7e6da81dd5f6d1ef9e11de72940d4c04ef4bba80
marcilustra <theknifebox69@gmail.com>
2024-10-30 13:41:36 +0200

    Leap year, Area of Circle, And so on

--- DSA-CAT1/DSA-CAT1.cpp
+++ DSA-CAT1/DSA-CAT1.cpp
@@ -10,9 +10,29 @@ float squareUp(float input) {
 float getCubed(float input) {
     return pow(input, 3);
 }
+double areaOfACircle(double radius) {
+    return 3.14159 * pow(radius, 2);
+}
+void isItLeap() {
+    int inYear;
+    cout << "Enter a year for me to check for leapness: ";
+    cin >> inYear;
+    if (inYear % 4 == 0) {
+        cout << "It is leap!";
+    }
+    else {
+        cout << "This year is not leap!";
+    }
+}
+
 int main()
 {
     int one = 12;
-    cout << squareUp(12) << endl;
-    cout << getCubed(12) << endl;
+    int two = 2004;
+    int three = 2023;
+    cout << squareUp(one) << endl;
+    cout << getCubed(one) << endl;
+    cout << "Area of a circle of radius " << one << " units" << areaOfACircle(one) << endl;
+
+    isItLeap();
 }
