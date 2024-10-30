de7245e87746b6f2117615a6b1205d94dba70ae2
marcilustra <theknifebox69@gmail.com>
2024-10-30 12:37:35 +0200

    Added the Monthly repayments calculator function

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Loan.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Loan.java
@@ -1,4 +1,5 @@
 package com.mycompany.mavenproject1;
+import static java.lang.System.out;
 
 /**
  *
@@ -7,9 +8,9 @@ package com.mycompany.mavenproject1;
 public class Loan {
     
     //Class Members
-    private double annualInterestRate;
-    private double loanAmount;
-    private int numberOfYears;
+    private double annualInterestRate = 2.5;
+    private double loanAmount = 1000;
+    private int numberOfYears = 1;
     
 //    private Date loanDate;
     
@@ -24,10 +25,44 @@ public class Loan {
 //    pub;ic Date getLoanDate();
     
     //Setters
-    public void setAnnualInterestRate(double annualInterestRate){
-        this.annualInterestRate = annualInterestRate;
+    public void setAnnualInterestRate(double AIR){
+        if(AIR < 2.5){
+            out.println("Let's have a reasonnable rate of interest");
+        }
+        else if(AIR > 20){
+            out.println("We are a bank not a crime syndicate");
+        }else{
+            this.annualInterestRate = AIR;
+        }
     }
-    public void
+    
+    public void setNumberOfYears(int years){
+        if(years < 1){
+            out.println("How do you expect payments to be made in less than one year");
+        }else{
+            this.numberOfYears = years;
+        }
+    }
+    public void setLoanAmount(double amount){
+        if(amount < 1000){
+            out.println("Amount has to be above $1000");
+        } else{
+            this.loanAmount = amount;
+        }
+    }
+    
+    public double monthlyPayment(){
+        double installment,rate,principle = 0;
+        principle = this.loanAmount;
+        rate = annualInterestRate / 12;
+        int months = numberOfYears * 12;
+        double ratePlusOne = rate + 1;
+        double expoRatePlusOne = Math.pow(ratePlusOne, months);
+        installment = principle * (rate * (expoRatePlusOne) / (expoRatePlusOne - 1));
+        out.println("Monthly Installments:" + installment);
+        
+        return installment;
+    }
         
     
 }
