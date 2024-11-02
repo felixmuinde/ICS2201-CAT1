dea7f592d265760a4212b3d00d8fe09c9003075b
marcilustra <theknifebox69@gmail.com>
2024-11-02 04:16:50 +0300

    Project setup

---
+++ Payrolling/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmPayroll
+    Inherits MaterialSkin.Controls.MaterialForm
+    Private connectionString = My.Settings.ConnectionString
+
+    Private Sub frmPayroll_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
