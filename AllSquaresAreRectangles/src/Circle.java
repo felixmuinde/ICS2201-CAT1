c6390f3dea23670344d6edbcfffcff5bca146da0
marcilustra <theknifebox69@gmail.com>
2024-12-01 18:15:27 +0200

    Added the document

--- AllSquaresAreRectangles/src/Circle.java
+++ AllSquaresAreRectangles/src/Circle.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
- * @author Administrator
+ * @author Felix Muinde
  */
 public class Circle extends Shape{
     private double radius;
