10424f0bc865bd0fd79453638a6dc235dcf741f2
marcilustra <theknifebox69@gmail.com>
2024-12-01 17:25:09 +0200

    Rectangle subclass

--- AllSquaresAreRectangles/src/Rectangle.java
+++ AllSquaresAreRectangles/src/Rectangle.java
@@ -7,6 +7,17 @@
  *
  * @author Administrator
  */
-public class Rectangle {
+public class Rectangle extends Shape{
+    private double length;
+    private double width;
     
+    public Rectangle(String color, double lenghth, double width){
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+    @Override
+    public double calculateArea(){
+        return length * width;
+    }
 }
